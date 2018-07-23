cb7e0f8a8d214ba2c019ee5bc57c62c909289581
zaneagley <zaneagley@gmail.com>
2018-07-23 10:20:20 -0500

    fixed some bugs with sorting and adding movies bringing up modals.

--- src/index.js
+++ src/index.js
@@ -234,17 +234,42 @@ $('#sortBy').change(function () {
         });
         let output = '';
 
-        movies.forEach(({title, rating, id, genre, image}) =>{
+        movies.forEach(({title, rating, id, genre, image}) => {
             output += '<div class="movieStats col-sm-12 col-md-3 border border-dark p-0"><marquee class="m-0">Title: ' + title + '</marquee>';
             output += '<p class="m-0">Rating: ' + rating + '</p>';
             output += '<p class="m-0">Genre: ' + genre + '</p>';
             output += '<p class="m-0">ID: ' + id + '</p>';
             output += '<button class="editBtn text-hide" data-toggle="modal" data-target="#editModal"><img src="edit.png"></button>';
             output += '<button class="deleteBtn btn"><img src="delete.png"></button><br>';
-            output += `<img class="poster" src="${image}"></div>`;
+            output += `<img class="poster" data-toggle="modal" data-target="#exampleModalCenter" src="${image}"></div>`;
+
 
             $('#bodyText').html(output);
 
+            $('.poster').click(function () {
+
+                //      $(this).parent().children().next().css('background-color', 'yellow');
+                let title = $(this).parent().children().first().html().slice(7);
+                console.log(title);
+
+                $.ajax(APIFront + title + APIBack).done((data) => {
+                    console.log(data);
+                    output = '';
+                    output += '<div><h3>' + title + '</h3>';
+                    output += `<img class="miniPoster" src="${data.Poster}">`;
+                    output += '<p class="text-left plotText"><strong>Plot:</strong> ' + data.Plot + '</p>';
+                    output += '<p><strong>Actors:</strong> ' + data.Actors + '</p>';
+                    output += '<p><strong>Runtime:</strong> ' + data.Runtime + '</p>';
+                    output += '<p><strong>Released:</strong> ' + data.Released + '</p></div>';
+
+
+                    console.log(output);
+
+                    $('.modal-info').html(output);
+
+                });
+
+            });
         });
         }
     )
